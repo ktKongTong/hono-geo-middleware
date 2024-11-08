7914c47966c95d0acbc75f446c8ffe48ddd10173
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-11-09 01:04:51 +0800

    deno

--- apps/geo-deno/main.ts
+++ apps/geo-deno/main.ts
@@ -7,7 +7,9 @@ app.use('/*', GeoMiddleware())
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-
+app.get('/header', (c) => {
+  return c.newResponse("", 200, c.req.header())
+})
 app.get('/geo', (c) => {
   return c.json(getGeo(c))
 })
